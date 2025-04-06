90a2208de062ffea128c068b070a16a2a6a4f600
Rahmat Dwi Yanto <80576861+zheonebro@users.noreply.github.com>
2025-04-06 16:52:46 +0700

    Update multi_sender_cli_v2.py

--- multi_sender_cli_v2.py
+++ multi_sender_cli_v2.py
@@ -224,7 +224,7 @@ def process_single_wallet(wallet):
             'nonce': nonce
         })
         signed_tx = w3.eth.account.sign_transaction(tx, private_key=PRIVATE_KEY)
-        tx_hash = w3.eth.send_raw_transaction(signed_tx['raw_transaction'])
+        tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
         tx_link = f"https://sepolia.tea.xyz/tx/{tx_hash.hex()}"
         logger.info(f"✅ Token {TOKEN_NAME} dikirim ke: {wallet} | Jumlah: 1 {TOKEN_NAME} | TX: {tx_link}")
         save_sent_wallet(wallet)
